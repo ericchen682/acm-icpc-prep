824d75794e13622a3419e86b350a7f223c88d1be
ericchen682 <eric.chen682@gmail.com>
2026-07-22 22:19:27 -0700

    solve week 1 noldbach problem

---
+++ week1/noldbach-problem.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // at least k prime numbers between 2 and n can be represented as the sum of 2 consecutive primes and 1
+    int n, k; cin >> n >> k;
+    vector<bool> prime(n+1, true);
+    vector<int> primes;
+    for(int i = 2; i <= n; i++)
+    {
+        if(!prime[i])
+            continue;
+        primes.push_back(i);
+        for(int j = i * 2; j <= n; j += i)
+            prime[j] = false;
+    }
+    for(int i = 0; i < primes.size() - 1; i++)
+    {
+        if(primes[i] + primes[i+1] + 1 > n)
+            break;
+        if(prime[primes[i] + primes[i+1] + 1])
+            k--;
+    }
+    if(k <= 0)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+}
