abaacbe16b28905c076f796478d51c90b3eb7165
ericchen682 <eric.chen682@gmail.com>
2026-07-22 21:54:12 -0700

    init repo with gitignore and solve week 1 tiny-implementation

---
+++ week1/tiny-implementation.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    int l, r;
+    cin >> l >> r;
+    vector<bool> res(r + 1, true);
+    res[0] = false;
+    res[1] = false;
+    for(int i = 2; i <= r; i++)
+    {
+        if(!res[i]) 
+            continue;
+        
+        for(int j = i * 2; j <= r; j += i)
+            res[j] = false;
+    }
+    for(int i = l; i <= r; i++)
+    {
+        if(res[i])
+            cout << i << ' ';
+    }
+}
