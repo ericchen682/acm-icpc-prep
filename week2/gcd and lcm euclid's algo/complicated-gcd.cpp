a3fd49b0d35472f832d84628d47dcfe997180b12
ericchen682 <eric.chen682@gmail.com>
2026-07-23 22:35:59 -0700

    solve week 2 complicated gcd

---
+++ week2/gcd and lcm euclid's algo/complicated-gcd.cpp
@@ -0,0 +1,13 @@
+// https://codeforces.com/contest/664/problem/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    string a, b;
+    cin >> a >> b;
+    if(a == b)
+        cout << a;
+    else
+        cout << 1;
+}
