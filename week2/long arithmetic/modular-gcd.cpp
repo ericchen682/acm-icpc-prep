25ce434dcef80813d5009876cd09e50dcc9a6b4a
ericchen682 <eric.chen682@gmail.com>
2026-07-26 17:32:49 -0700

    refactor: write mulmod and modpow funcs for week 2 modular gcd

--- week2/long arithmetic/modular-gcd.cpp
+++ week2/long arithmetic/modular-gcd.cpp
@@ -1,59 +1,70 @@
 // https://www.codechef.com/AUG18B/problems/GCDMOD
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include <numeric>
 using namespace std;
+
+// gcd of a^n + b^n and a - b (a always >= b, so no need for absolute value)
+// let diff = a - b
+
+// because we have a % diff = b % diff
+// we also have a^n % diff = b^n % diff
+// thus we have (a^n + b^n) mod diff = 2*(b^n) mod diff
+
+// thus our result is gcd(2*b^n mod diff, diff)
+// find 2*b^n mod diff through modular exponentiation
+
+// returns (a*b)%m w/ binary multiplication to prevent overflow
+long long mulmod(long long a, long long b, long long m)
+{
+    long long res = 0;
+    a %= m;
+    b %= m;
+    while(b > 0)
+    {
+        if(b & 1)
+        {
+            res += a;
+            res %= m;
+        }
+        a *= 2;
+        a %= m;
+        b = b >> 1;
+    }
+    return res;
+}
+
+// returns (base^exp)%m using binary exponentiation to prevent overflow
+long long modpow(long long base, long long exp, long long m)
+{
+    long long res = 1;
+    base %= m;
+    while (exp > 0)
+    {
+        if (exp & 1) res = mulmod(res, base, m);
+        base = mulmod(base, base, m);
+        exp >>= 1;
+    }
+    return res;
+}
 
 void solve()
 {
     long long a, b, n; cin >> a >> b >> n;
-    // gcd of a^n + b^n and a - b (a always >= b)
-    // let diff = a - b
-
-    // because we have a % diff = b % diff
-    // we also have a^n % diff = b^n % diff
-    // thus we have (a^n + b^n) mod diff = 2*(b^n) mod diff
-
-    // thus our result is gcd(2*b^n mod diff, diff)
-    // find 2*b^n mod diff through modular exponentiation
-
-    // requires casting to 128 bit int due to 10^24 not fitting within long long type
-
     long long diff = a-b;
 
     const long long MOD = 1e9 + 7;
-    // handle a = b edge case, simply calculate a^n + b^n mod 1e9 + 7
+    // handle a = b edge case, simply calculate a^n + b^n mod 1e9 + 7 because gcd(a^n + b^n, 0) = a^n + b^n
     if(diff == 0)
     {
-        long long resA = 1, resB = 1;
-        while(n > 0)
-        {
-            if(n % 2 == 1)
-            {
-                resA = (__int128(resA) * __int128(a)) % MOD;
-                resB = (__int128(resB) * __int128(b)) % MOD;
-            }
-            
-            n /= 2;
-            a = (__int128(a) * __int128(a)) % MOD;
-            b = (__int128(b) * __int128(b)) % MOD;
-        }
+        long long resA = modpow(a, n, MOD);
+        long long resB = modpow(b, n, MOD);
         cout << (resA + resB) % MOD << '\n';
         return;
     }
 
     // calculate 2*b^n mod diff
-
-    long long res = 1;
-    while(n > 0)
-    {
-        if(n % 2 == 1)
-            res = (__int128(res) * __int128(b)) % diff;
-        n /= 2;
-        b = (__int128(b) * __int128(b)) % diff;
-    }
-    
-    res *= 2;
-
+    long long res = 2 * modpow(b, n, diff) % diff;
     cout << gcd(res, diff) % MOD << '\n';
 }
 
